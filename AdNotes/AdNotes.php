522044a9cf2e767466c8fc9e5ecdad1faca1ab82
ovillani <o.villani@sdi-research.at>
2018-03-02 18:04:43 +0100

    Update AdNotes.php

--- AdNotes/AdNotes.php
+++ AdNotes/AdNotes.php
@@ -1,64 +1,70 @@
 <?php
-    /**
-	*	LS 3.x plugin try
-	*   Gives the possibility to add a js script to a question on certain places
-	*   based on Denis Chenu <denis@sondages.pro> addScriptToQuestion (not working in LS3.x)
-	*
-	*	added: switch of/on execution on question level
-	*	added: switch on/of execution on global (ls installation) level
-	*   added: default placement on global level
-	*
-	*	consider: switch on/of execution on survey level?
-	*   consider: more than one script per question (on different places)?
-	*
-	*	status: experimental
-	*			lots of todo's ;)
-	*/
+
+/**
+ * Gives the possibility to add some notes to a question
+ * A simple addon, but useful for workflow and documentation.
+ *
+ * @author Oskar Villani (o.villani@sdi-research.at)
+ * @copyright 2018 Oskar Villani <http://www.sdi-research.at>
+ * @license AGPL v3
+ * @version 1.0.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+class AddNotes extends PluginBase {							// class name fits to file name
 	
-	class AdNotes extends PluginBase {
-	
-        static protected $name 		  = 'AdNotes';
-		static protected $description = 'Allow to add notes to questions.';
+	static protected $name = 'AddNotes';						// as shown in plugin manager
+	static protected $description = 'Allow to add notes to questions.';		// as shown in plugin manager
         
-		protected $storage = 'DbStorage';
+	protected $storage = 'DbStorage';						// to save notes permanently
 		
-		/**
-		* Add function to be used in beforeQuestionRender event and to attriubute
-		*/
-		public function init()
-		{
-			$this->subscribe('newQuestionAttributes');			// attributes on global level for all surveys
-			$this->subscribe('beforeQuestionRender');			// attributes on question level 
-		}
-		
-		/**
-		* We add the attribute here
-		*/
-		public function newQuestionAttributes()
-		{
-			$event = $this->event;
-			$questionAttributes = array (
-				'adNoteText' => array(
-					'types'		=> '15ABCDEFGHIKLMNOPQRSTUWXYZ!:;|*', /* All question types */
-					'category'  => gT('AdNotes'),
-					'sortorder' => 1,
-					'inputtype' => 'textarea',
-					'default'   => '',
-					'caption'	=> 'Notes',
-					'help'		=> 'Edit your notes here'
-				),
-				'adToDoText' => array(
-					'types'		=> '15ABCDEFGHIKLMNOPQRSTUWXYZ!:;|*', /* All question types */
-					'category'  => gT('AdNotes'),
-					'sortorder' => 2,
-					'inputtype' => 'textarea',
-					'default'   => '',
-					'caption'	=> 'ToDo',
-					'help'		=> "Edit ToDos here."
-				)
-			);
-				
-			$event->append('questionAttributes', $questionAttributes);
-		}
-		
+	/**
+	* Add function to be used in beforeQuestionRender event and to attriubute
+	*/
+	public function init()
+	{
+		$this->subscribe('newQuestionAttributes');				// subscribe new attributes (i.e. fields
+											//                  in questions options)
 	}
+		
+	/**
+	* add the attributes on question level here
+	*/
+	public function newQuestionAttributes()
+	{
+		// define the fields
+		$questionAttributes = array (
+			'addNoteText' => array(						// one field for notes
+				'types'	    => '15ABCDEFGHIKLMNOPQRSTUWXYZ!:;|*', 	// for all question types 
+				'category'  => gT('AddNotes'),				// option category
+				'sortorder' => 1,					// show at first
+				'inputtype' => 'textarea',				// as a textarea
+				'default'   => '',					// empty by default
+				'caption'   => 'Notes',					// field name above textfield
+				'help'	    => 'Edit your notes here'			// some hint when hovering above caption
+			),
+			'addToDoText' => array(						// second field for todos
+				'types'	    => '15ABCDEFGHIKLMNOPQRSTUWXYZ!:;|*', 	// for all question types 
+				'category'  => gT('AddNotes'),
+				'sortorder' => 2,
+				'inputtype' => 'textarea',
+				'default'   => '',
+				'caption'   => 'ToDo',
+				'help'	    => "Edit ToDos here."
+			)
+		);
+				
+		$event = $this->event;							// get the event ...
+		$event->append('questionAttributes', $questionAttributes);		// ... and append the new fields
+	}
+		
+}
